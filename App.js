349758f21ea00cc055cebf934381d45b930670e7
JGITONGA <jimgitongajm@gmail.com>
2022-07-10 15:48:13 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -7,8 +6,8 @@
  * @flow strict-local
  */
 
- import * as React from 'react';
- import {
+import * as React from 'react';
+import {
   StatusBar,
   Image,
   Animated,
@@ -16,65 +15,73 @@
   View,
   Dimensions,
   StyleSheet,
- } from 'react-native';
- import {faker} from '@faker-js/faker';
- const {width, height} = Dimensions.get('screen');
- faker.seed(10);
- const DATA = [...Array(30).keys()].map((_, i) => {
-   return {
-     key: faker.datatype.uuid(),
-     image: `https://randomuser.me/api/portraits/${faker.helpers.arrayElement([
-       'women',
-       'men',
-     ])}/${faker.datatype.number(60)}.jpg`,
-     name: faker.name.findName(),
-     jobTitle: faker.name.jobTitle(),
-     email: faker.internet.email(),
-   };
- });
- 
- const SPACING = 20;
- const AVATAR_SIZE = 70;
- const ITEM_SIZE = AVATAR_SIZE + SPACING * 3;
- const BG_IMG =
-   'https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
- 
- export default function App() {
-   console.log(DATA.length);
-   const scrollY = React.useRef(new Animated.Value(0)).current;
-   return (
-     <View style={{flex: 1, backgroundColor: '#fff'}}>
-       <Image
-         source={{uri: BG_IMG}}
-         style={StyleSheet.absoluteFillObject}
-         blurRadius={80}
-       />
- 
-       <Animated.FlatList
-         data={DATA}
-         contentContainerStyle={{
-           padding: SPACING,
-           paddingTop: StatusBar.currentHeight || 42,
-         }}
-         keyExtractor={item => item.key.toString()}
-         onScroll={Animated.event(
+} from 'react-native';
+import {faker} from '@faker-js/faker';
+const {width, height} = Dimensions.get('screen');
+faker.seed(10);
+const DATA = [...Array(30).keys()].map((_, i) => {
+  return {
+    key: faker.datatype.uuid(),
+    image: `https://randomuser.me/api/portraits/${faker.helpers.arrayElement([
+      'women',
+      'men',
+    ])}/${faker.datatype.number(60)}.jpg`,
+    name: faker.name.findName(),
+    jobTitle: faker.name.jobTitle(),
+    email: faker.internet.email(),
+  };
+});
+
+const SPACING = 20;
+const AVATAR_SIZE = 70;
+const ITEM_SIZE = AVATAR_SIZE + SPACING * 2;
+const BG_IMG =
+  'https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
+
+export default function App() {
+  console.log(DATA.length);
+  const scrollY = React.useRef(new Animated.Value(0)).current;
+  const [heightstate, heightstateSet] = React.useState(0);
+  const onLayout = event => {
+    const {x, y, height, width} = event.nativeEvent.layout;
+    heightstateSet(height);
+    console.log('x', x, 'y', y, 'height', height, 'width', width);
+  };
+  return (
+    <View style={{flex: 1, backgroundColor: '#fff'}}>
+      <Image
+        source={{uri: BG_IMG}}
+        style={StyleSheet.absoluteFillObject}
+        blurRadius={80}
+      />
+
+      <Animated.FlatList
+        // ref={scrollY}
+        data={DATA}
+        contentContainerStyle={{
+          padding: SPACING,
+          paddingTop: StatusBar.currentHeight || 42,
+          marginBottom: SPACING,
+        }}
+        keyExtractor={item => item.key}
+        onScroll={Animated.event(
           [{nativeEvent: {contentOffset: {y: scrollY}}}],
           {
             useNativeDriver: true,
           },
         )}
-         renderItem={({item, index}) => {
+        renderItem={({item, index}) => {
           const inputRange = [
             -1,
             0,
-            ITEM_SIZE * index,
-            ITEM_SIZE * (index + 2),
+            heightstate * index,
+            heightstate * (index + 2),
           ];
           const opacityInputRange = [
             -1,
             0,
-            ITEM_SIZE * index,
-            ITEM_SIZE * (index + 1),
+            heightstate * index,
+            heightstate * (index + 1),
           ];
 
           const scale = scrollY.interpolate({
@@ -82,45 +89,44 @@
             outputRange: [1, 1, 1, 0],
           });
           const opacity = scrollY.interpolate({
-            opacityInputRange,
+            inputRange: opacityInputRange,
             outputRange: [1, 1, 1, 0],
           });
-        // 
-           return (
-             <Animated.View
-               key={index}
-               style={{
-                 flexDirection: 'row',
-                 padding: SPACING,
-                 marginBottom: SPACING,
-                 backgroundColor: 'rgba(255,255,255,0.8)',
-                 borderRadius: 12,
-                 elevation: 3,
-                
-               }}>
-               <Image
-                 source={{uri: item.image}}
-                 style={{
-                   width: AVATAR_SIZE,
-                   height: AVATAR_SIZE,
-                   borderRadius: AVATAR_SIZE,
-                   margin: SPACING / 2,
-                 }}
-               />
-               <View>
-                 <Text style={{fontSize: 22, fontWeight: '700'}}>
-                   {item.name}
-                 </Text>
-                 <Text style={{fontSize: 18, opacity: 0.7}}>
-                   {item.jobTitle}
-                 </Text>
-                 <Text style={{fontSize: 22, opacity: 0.8}}>{item.email}</Text>
-               </View>
-             </Animated.View>
-           );
-         }}
-       />
-     </View>
-   );
- }
- 
+          return (
+            <Animated.View
+              onLayout={onLayout}
+              style={{
+                flexDirection: 'row',
+                padding: SPACING,
+                marginBottom: SPACING,
+                backgroundColor: 'rgba(255,255,255,0.8)',
+                borderRadius: 12,
+                elevation: 3,
+                opacity,
+                transform: [{scale}],
+              }}>
+              <Image
+                source={{uri: item.image}}
+                style={{
+                  width: AVATAR_SIZE,
+                  height: AVATAR_SIZE,
+                  borderRadius: AVATAR_SIZE,
+                  margin: SPACING / 2,
+                }}
+              />
+              <View>
+                <Text style={{fontSize: 22, fontWeight: '700'}}>
+                  {item.name}
+                </Text>
+                <Text style={{fontSize: 18, opacity: 0.7}}>
+                  {item.jobTitle}
+                </Text>
+                <Text style={{fontSize: 22, opacity: 0.8}}>{item.email}</Text>
+              </View>
+            </Animated.View>
+          );
+        }}
+      />
+    </View>
+  );
+}
